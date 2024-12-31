0f8f3ce96705a20f2462758dfb6f95b1fead1f8f
BugHunter-X <askzenai@gmail.com>
2024-12-31 21:02:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,13 @@
+```javascript
+// pages/index.js
+
+export default function Home() {
+  //If the issue persist, try adding this line
+  //const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+    </div>
+  );
+}
+```
